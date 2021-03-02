68b23705ed8722790670a384162f82a08760238b
Danil <danikavsilev@gmail.com>
2021-03-02 12:07:24 +0300

    Подключение базы данных

---
+++ NewsSiteProject/Data/ApplicationContext .cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Entities;
+
+namespace Data
+{
+    public class ApplicationContext : DbContext 
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+           : base(options) {}
+        
+        public DbSet<News> News { get; private set; } 
+    }
+}
