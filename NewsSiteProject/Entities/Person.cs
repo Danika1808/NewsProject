cab4d4e0acb98aa7132549205cfce74e96debcd1
Danil <danikavsilev@gmail.com>
2021-03-02 12:08:40 +0300

    Класс персональных данных

---
+++ NewsSiteProject/Entities/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Person
+    {
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public string DateOfBirth { get; private set; }
+    }
+}
