51ae116f23eef9cf3fb2ac45070767629217d37f
Danil <danikavsilev@gmail.com>
2021-03-02 12:08:08 +0300

    Класс логина и пароля

---
+++ NewsSiteProject/Entities/LoginPassword.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Entities
+{
+    public class LoginPassword
+    {
+        public Guid Id { get; private set; }
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public Guid UserID { get; private set; }
+
+        private LoginPassword() { }
+
+        public LoginPassword(Guid id, string login, string password, Guid userId)
+        {
+            Id = id;
+            Login = login;
+            Password = password;
+            UserID = userId;
+        }
+        public static void ChangePassword() 
+        {
+            
+        }
+
+        public static bool IsVerified(string hashedPassword, string password)
+        {
+            byte[] buffer4;
+            if (hashedPassword == null)
+            {
+                return false;
+            }
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            byte[] src = Convert.FromBase64String(hashedPassword);
+            if ((src.Length != 0x31) || (src[0] != 0))
+            {
+                return false;
+            }
+            byte[] dst = new byte[0x10];
+            Buffer.BlockCopy(src, 1, dst, 0, 0x10);
+            byte[] buffer3 = new byte[0x20];
+            Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8))
+            {
+                buffer4 = bytes.GetBytes(0x20);
+            }
+            return buffer3.SequenceEqual(buffer4);
+        }
+
+        public static string HashPassword(string password)
+        {
+            byte[] salt;
+            byte[] buffer2;
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
+            {
+                salt = bytes.Salt;
+                buffer2 = bytes.GetBytes(0x20);
+            }
+            byte[] dst = new byte[0x31];
+            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+            return Convert.ToBase64String(dst);
+        }
+    }
+}
