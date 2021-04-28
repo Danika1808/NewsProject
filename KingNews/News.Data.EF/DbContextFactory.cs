64bf7c447d3042a84ac8514682bc920eea34b5dd
Danil <danikavsilev@gmail.com>
2021-04-28 05:37:34 +0300

    initial create

---
+++ KingNews/News.Data.EF/DbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace News.Data.EF
+{
+    class DbContextFactory
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public DbContextFactory(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public AppDbContext Create(Type repositoryType)
+        {
+            var services = _httpContextAccessor.HttpContext.RequestServices;
+
+            var dbContexts = services.GetService<Dictionary<Type, AppDbContext>>();
+            if (!dbContexts.ContainsKey(repositoryType))
+                dbContexts[repositoryType] = services.GetService<AppDbContext>();
+
+            return dbContexts[repositoryType];
+        }
+    }
+}
---
+++ KingNews/News.Data.EF/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
+
+namespace News.Data.EF
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEfRepositories(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<AppDbContext>(
+                options =>
+                {
+                    options.UseNpgsql(connectionString);
+                },
+                ServiceLifetime.Transient
+            );
+
+            services.AddScoped<Dictionary<Type, AppDbContext>>();
+
+            return services;
+        }
+    }
+}
