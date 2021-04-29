fceb1a85955acc8300283b139ce1b6762fecba62
Timofey Borisov <borisovtim@mail.ru>
2021-04-29 06:56:57 +0300

    Comment

---
+++ KingNews/News.Data.EF/Repository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace News.Data.EF
+{
+    class Repository<T> : IRepository<T> where T : class
+    {
+        DbContext _context;
+        DbSet<T> _dbSet;
+
+        private readonly DbContextFactory _dbContextFactory;
+
+        
+
+        public Repository(DbContext context, DbContextFactory dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _dbSet.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<T> Get(Func<T, bool> predicate)
+        {
+            return _dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+        public T FindById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Create(T item)
+        {
+            _dbSet.Add(item);
+            _context.SaveChanges();
+        }
+        public void Update(T item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        public void Remove(T item)
+        {
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetWithInclude(params Expression<Func<T, object>>[] includeProperties)
+        {
+            return Include(includeProperties).ToList();
+        }
+
+        public IEnumerable<T> GetWithInclude(Func<T, bool> predicate,
+            params Expression<Func<T, object>>[] includeProperties)
+        {
+            var query = Include(includeProperties);
+            return query.Where(predicate).ToList();
+        }
+
+        private IQueryable<T> Include(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _dbSet.AsNoTracking();
+            return includeProperties
+                .Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+    }
+}
---
+++ KingNews/News.Web.App/Models/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Web.App
+{
+    class Comment
+    {
+        public Guid Id { get; protected set; }
+
+        [MaxLength(20, ErrorMessage = "Line length exceeded")]
+        public string Content { get; protected set; }
+
+        public DataType date_of_publication { get; protected set; }
+
+         [ForeignKey("AspNetUsers")]
+        public User User { get; protected set; }
+
+
+    }
+}
