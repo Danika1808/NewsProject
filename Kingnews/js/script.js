3488d6bfa329ef00597d03745b74e0adc412e78b
mart <safonova75@icloud.com>
2021-05-20 21:29:23 +0300

    contact page

--- Kingnews/js/script.js
+++ Kingnews/js/script.js
@@ -11,3 +11,76 @@ $(document).ready(function () {
         $(".popular-news .tab-form").eq($(this).index()).addClass("active-form");
     });
 });
+ 
+$(document).ready(function() {
+    $("#contact-name").on("focus", function () {
+        $("#contact-name-label").addClass("focus");
+        $(this).removeClass("invalid");
+    });
+    
+});
+$(document).ready(function() {
+    $("#contact-name").blur(function() {
+        if(!$(this).val()) {
+            $("#contact-name-label").removeClass("focus");
+            $(this).addClass("invalid");
+        } else {
+            $(this).removeClass("invalid");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#contact-email").on("focus", function () {
+        $("#contact-email-label").addClass("focus");
+        $(this).removeClass("invalid");
+    });
+
+});
+$(document).ready(function() {
+    $("#contact-email").blur(function() {
+        if(!$(this).val()) {
+            $("#contact-email-label").removeClass("focus");
+            $(this).addClass("invalid");
+        } else {
+            $(this).removeClass("invalid");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#your-website").on("focus", function () {
+        $("#your-website-label").addClass("focus");
+        $(this).removeClass("invalid");
+    });
+
+});
+$(document).ready(function() {
+    $("#your-website").blur(function() {
+        if(!$(this).val()) {
+            $("#your-website-label").removeClass("focus");
+            $(this).addClass("invalid");
+        } else {
+            $(this).removeClass("invalid");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#message").on("focus", function () {
+        $("#message-label").addClass("focus");
+        $(this).removeClass("invalid");
+    });
+
+});
+$(document).ready(function() {
+    $("#message").blur(function() {
+        if(!$(this).val()) {
+            $("#message-label").removeClass("focus");
+            $(this).addClass("invalid");
+        } else {
+            $(this).removeClass("invalid");
+        }
+    });
+});
+
